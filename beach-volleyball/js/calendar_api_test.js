b0f9c839346887d3e948477dc15e2a3760216304
manos <emanon1989@gmail.com>
2022-10-12 22:25:25 +0100

    adding calendar api from stagging-env

---
+++ beach-volleyball/js/calendar_api_test.js
@@ -0,0 +1,7 @@
+function httpGet(theUrl) {
+    let xmlHttpReq = new XMLHttpRequest();
+    xmlHttpReq.open("GET", theUrl, false); 
+    xmlHttpReq.send(null);
+    return xmlHttpReq.responseText;
+  }
+   console.log(httpGet('https://registration-stagging.sideout.co.uk/calendars'));
